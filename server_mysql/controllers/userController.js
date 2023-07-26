7fb7e7b4a22f7b872ba8a657e500e6e03acdb2e5
MarkusNodeJs <markus.dalton03282023@gmail.com>
2023-07-26 14:55:38 +0800

    nodejs

--- server_mysql/controllers/userController.js
+++ server_mysql/controllers/userController.js
@@ -3,27 +3,67 @@ const db = con;
 
 /** HTTP GET METHOD - get all data */
 const getUsers = async (req, res) => {
-  res.send("Get Users");
+  const sql = "SELECT * FROM users";
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 };
 
 /** HTTP POST METHOD - save data */
 const createUser = async (req, res) => {
-  res.send("Create User");
+  const { name, email, age } = req.body;
+  const sql = "INSERT INTO users(name, email, age) values (?, ?, ?)";
+  db.query(sql, [name, email, age], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(200).json({ message: "User added successfully" });
+    }
+  });
 };
 
 /** HTTP GET METHOD - get single data */
 const getUser = async (req, res) => {
-  res.send("Get User");
+  const { id } = req.params;
+  const sql = "SELECT * FROM users WHERE id = ?";
+  db.query(sql, id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 };
 
 /** HTTP PUT METHOD - update selected data */
 const updateUser = async (req, res) => {
-  res.send("Update User");
+  const { id } = req.params;
+  const { name, email, age } = req.body;
+  const sql = "UPDATE users SET name = ?, email = ?, age = ? WHERE id = ?";
+  db.query(sql, [name, email, age, id], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(201).json({ message: "Updated Successfully" });
+    }
+  });
 };
 
 /** HTTP DELETE METHOD - delete selected data */
 const deleteUser = async (req, res) => {
-  res.send("Delete User");
+  const { id } = req.params;
+  const sql = "DELETE FROM users WHERE id = ?";
+  db.query(sql, id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(201).json({ message: "Deleted Successfully" });
+    }
+  });
 };
 
 export { getUsers, getUser, createUser, updateUser, deleteUser };
